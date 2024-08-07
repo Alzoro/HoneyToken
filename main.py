e20133632138f2bc4cf989b67f8e74f59d7d9d51
Alzoro <jones.200423@gmail.com>
2024-08-07 17:43:05 +0530

    deploy part 50% completed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import typer as t
 from art import text2art
 from termcolor import colored
+import os
+from pathlib import Path
+
 
 app=t.Typer()
 
@@ -11,6 +14,8 @@ def intractive_mode():
         command=input(htok)
         if command == "-help":
             help()
+        elif command == "-deploy":
+            honeytoken_deploy()
         elif command.lower() == "exit":
             break
         else:
@@ -32,6 +37,97 @@ def pre_info():   #Info displayed at the beging of the tool
     t.echo(colored(te,color="green"))
     t.echo(colored("\t\t< ","green") + colored("-help ","yellow") + colored("for help or '","green" ) + colored("exit","yellow") + colored("' to quit >\n","green"))
 
+def honeytoken_deploy():
+
+    while True:
+        p_input=input("Specify the path (absolute path): ")
+        path=Path(p_input)
+
+        if p_input == "exit":
+            break
+
+        if not path.is_absolute():
+            t.echo(colored("\n\t\tPlease provide the absolute path\n","yellow"))
+        else:
+            break
+    
+    if not path.parent.exists():
+        try:
+            path.mkdir(parents=True,exist_ok=True)
+            t.echo("\nCreated directories for the path: " + colored(path,"magenta") + "\n")
+        except Exception as e:
+            t.echo(colored(f"An error occured while creating directories: {e}","yellow"))
+    elif path.exists():
+        t.echo("\nCurrent path: " + colored(path,"magenta") + "\n")
+    else:
+        path.mkdir(exist_ok=True)
+        t.echo(f"Created directories for the path: {path}")
+
+    t.echo(colored("\t\tSupported file types","green"))
+    t.echo(colored("\t\t-[","green") + colored(".txt ","magenta")+ colored("]-  Text File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".conf","magenta")+ colored("]-  Configuration File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".ini ","magenta")+ colored("]-  Initialization File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".docx","magenta")+ colored("]-  Microsoft Word Document (Open XML Document)","green"))
+    t.echo(colored("\t\t-[","green") + colored(".sql ","magenta")+ colored("]-  Structured Query Language File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".db  ","magenta")+ colored("]-  Database File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".env ","magenta")+ colored("]-  Environment File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".json","magenta")+ colored("]-  JavaScript Object Notation File","green"))
+    t.echo(colored("\t\t-[","green") + colored(".log ","magenta")+ colored("]-  Log File","green"))
+
+    f_name=input("File name (with extension): ")
+    f_create(f_name,path)
+        
+
+
+def f_create(f_name,path):
+    ext=Path(f_name).suffix
+    t.echo(colored("\n\t\t-[","green") + colored("-d","magenta") + colored("]- for default content"))
+    t.echo(colored("\t\t-[","green") + colored("-c","magenta") + colored("]- for custom content\n"))
+    if ext == ".txt":
+        default="""# Sensitive Information
+
+Username: admin
+Password: P@ssw0rd1234
+
+API Key: 12345-abcde-67890-fghij
+Secret Token: 98765-zyxwv-43210-lmnop
+"""
+        try:
+            with open(path/f_name,"w+") as file:
+                while True:
+                    ct=input("Select one: ")
+                    if ct == "-d":
+                        file.write(default)
+                        t.echo("\nFile created\nFile content:\n")
+                        file.seek(0)
+                        c=file.read()
+                        t.echo(c)
+                        break
+                    elif ct == "-c":
+                        file.write(custom())
+                        t.echo("\nFile created\nFile content:\n")
+                        file.seek(0)
+                        c=file.read()
+                        t.echo(c)
+                        break
+                    else:
+                        t.echo(colored("\n\t\tInvalid input\n","yellow"))
+        except Exception as e:
+            t.echo(f"An error occurred: {e}")
+                
+
+
+        
+def custom():
+    t.echo("Enter your custom content (type '-END' on a new line to finish)\n")
+    lines=[]
+    while True:
+        l=input()
+        if l == "-END" or l=="-end":
+            break
+        lines.append(l)
+    
+    return '\n'.join(lines)
 
 
 @app.command()
